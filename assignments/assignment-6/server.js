f924ef7c93c03f28b66e0af2e1dbc867002cffd7
Tohir Kuliev <kuliev.tech@gmail.com>
2023-09-18 12:13:25 -0400

    finish assignment-6

---
+++ assignments/assignment-6/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { carRouter } from "./routes/car.routes.js";
+import { maintenanceRouter } from "./routes/maintenance.route.js";
+import { rentalRouter } from "./routes/rental.route.js";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 4040;
+
+app.use("/cars", carRouterRouter);
+app.use("/maintenances", maintenanceRouter);
+app.use("/rentals", rentalRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
